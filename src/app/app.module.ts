54a6f526c4ac94d8061401fc2a31f26885240b19
Adam Smith <adamws33@gmail.com>
2018-06-07 13:34:39 -0400

    before connecting to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+const firebaseConfig  = {
+  apiKey: "AIzaSyAC9fE52KFSWs-wVkW53KagXET-UOebizc",
+  authDomain: "simpleprojectangular.firebaseapp.com",
+  databaseURL: "https://simpleprojectangular.firebaseio.com",
+  projectId: "simpleprojectangular",
+  storageBucket: "simpleprojectangular.appspot.com",
+  messagingSenderId: "306773474043"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+
+  submitInfo = "This will update once submitted"
+  infoUp = "Hello"
+  private _infoform: FormGroup;
+  
+
+  constructor(private _form: FormBuilder) { 
+    this.createForm()
+  }
+
+  ngOnInit() {
+  }
+
+  createForm(){
+    this._infoform = this._form.group({
+      Info: new FormControl
+    })
+  }
+
+  updateInfo(){
+    console.log(this._infoform.value.Info)
+    this.infoUp = this._infoform.value.Info
+  }
+
+  Submit(){
+    this.submitInfo = this._infoform.value.Info
+  }
+
+}
